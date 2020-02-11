c09a48f79c376e121c9b0d44b1163e2a0bce1812
mauriciomarcos.consultoria@gmail.com <mauriciomarcos.consultoria@gmail.com>
2020-02-10 21:30:33 -0300

    Adicionado e configurado Swashbuckle.AspNetCore (Swagger) ao projeto.

--- ClashRoyaleAPI/Startup.cs
+++ ClashRoyaleAPI/Startup.cs
@@ -1,10 +1,14 @@
-﻿using ClashRoyaleService;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using ClashRoyaleService;
 using ClashRoyaleService.ServiceInterfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace ClashRoyaleAPI
 {
@@ -23,7 +27,9 @@ namespace ClashRoyaleAPI
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddSingleton<IConfiguration>(Configuration);
 
-            services.AddSingleton<ICardService, CardService>();
+            CarregarDependenciasDominioAplicacao(services);
+            ConfigurarDependenciasSwagger(services);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +46,38 @@ namespace ClashRoyaleAPI
             }
 
             app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(config => {
+                config.SwaggerEndpoint("/swagger/Doc.v1.0/swagger.json", "Open API Clash Royale - Versão 1.0");
+            });
             app.UseMvc();
         }
+
+        private void CarregarDependenciasDominioAplicacao(IServiceCollection services)
+        {
+            services.AddSingleton<ICardService, CardService>();
+        }
+
+        private void ConfigurarDependenciasSwagger(IServiceCollection services)
+        {
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = $"{Path.Combine(AppContext.BaseDirectory, xmlFile)}";
+
+            services.AddSwaggerGen(config => {
+                config.SwaggerDoc("Doc.v1.0", new OpenApiInfo() {
+                    Title = "Open API Clash Royale",
+                    Version = "v1.0",
+                    Description = "API aberta do Clash Royale. Recursos aberto para integração com sites e outros formatos de mídias relacionado ao jogo Clash Royale.",
+                    Contact = new OpenApiContact() {
+                        Name = "Maurício Marcos",
+                        Email = "mauriciomarcos.consultoria@gmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/mmarcos001") 
+                    },
+                    TermsOfService = new Uri("https://github.com/GITMMarcos/clashroyale-api")
+                });
+                config.IncludeXmlComments(xmlPath);
+            });
+        }
+
     }
 }
